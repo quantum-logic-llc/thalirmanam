d85899f40a0d1d064e72bf8f70bbc78251e05477
Muthu V <v.muthamizh@gmail.com>
2025-12-29 15:42:36 +0530

    spell check

--- src/components/sections/footers/copyRight.jsx
+++ src/components/sections/footers/copyRight.jsx
@@ -8,7 +8,7 @@ const CopyRight = ({ color }) => {
             <div className="flex lg:flex-row flex-col justify-between lg:items-center pt-7.5 pb-8 border-t border-t-white border-opacity-20">
                 <p className={cn('wow fadeInLeft', color)} data-wow-delay=".3s">© <Link href="#">Thalir Manam</Link> 2025 | All Rights Reserved</p>
                 <ul className="flex items-center gap-7.5 wow fadeInRight" data-wow-delay=".3s">
-                    <li><Link href="#" className={cn(``, color)}>Trams & Condition</Link></li>
+                    <li><Link href="#" className={cn(``, color)}>Terms & Condition</Link></li>
                     <li><Link href="#" className={cn(``, color)}>Privacy Policy</Link></li>
                     <li><Link href="#" className={cn(``, color)}>Contact Us</Link></li>
                 </ul>
