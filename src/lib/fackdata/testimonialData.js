ca6b3bcfbb1de4ac0e8c92aaa892ceba8a5c5ee4
Karthik0008 <144876341+Karthik0008@users.noreply.github.com>
2026-01-12 16:20:00 +0530

    review update

--- src/lib/fackdata/testimonialData.js
+++ src/lib/fackdata/testimonialData.js
@@ -32,7 +32,7 @@ export const testimonialData = [
         src:user_3,
         name:"shikha rajeev",
         position:"Chennai",
-        review: "We consulted Dr Naveena about our son.Having met a lot of doctors about our son she stood out as someone truly exceptional and caring. ",
+        review: "We consulted Dr Naveena about our son.Having met a lot of doctors about our son she stood out as someone truly exceptional and caring.She has a very compassionate and wonderful approach. She listened to our problems and very patiently explained the treatment plan. She made us feel confident and informed about our son's diagnosis.She guided us patiently and carefully outlined our treatment options. Under her care and guidance our son is showing wonderful improvement. She reaches out to us to know about therapy review and gives a lot of helpful suggestions to improve him.There are monthly reviews about the goals achieved and she discusses the same with the therapists as well. We are genuinely happy that we met her at the crucial time and steered us in the right direction. Dr Naveena has made a positive difference in our lives ",
         rating:5
     },
     {
@@ -57,6 +57,14 @@ export const testimonialData = [
         name:" Vaji Azeem",
         position:"Chennai",
         review:"Dr. Naveena was very patient and caring with my child. She spoke directly with my child, encouraged my child to talk, and involved my child in activities. She carefully observed my child’s behavior and explained her observations clearly.",
+        rating:5
+    },
+    {
+        id:8,
+        src:user_3,
+        name:"Anbu Selvi",
+        position:"Chennai",
+        review:"My son is 14 years old, he has a Autism and has teenage behavior issues along with many doubts and confusions. We consulted Dr. Naveena Karthick at MIOT Hospital.She gave us very good clarification and clear guidance. She explained how to work with our son and what steps we should take for the next goals. Her approach towards my son was very kind, calm, and understanding. We are very satisfied with the consultation and truly appreciate her support and guidance. So, I genuinely recommend to her everyone.",
         rating:5
     }
 
